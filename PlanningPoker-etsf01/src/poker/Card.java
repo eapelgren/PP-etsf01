05b2ba071b358f9c59532c4b01500b8e4b3aa333
Josefin Karlsson <josefin.karlsson@live.se>
2013-04-15 09:52:11 +0200

    Card och CardDeck

--- PlanningPoker-etsf01/src/poker/Card.java
+++ PlanningPoker-etsf01/src/poker/Card.java
@@ -1,5 +1,24 @@
 package poker;
 
-public class Card {
+public class Card implements Card_Interface {
+	private int value;
+	
+	public Card(int value){
+		this.value=value;
+	}
+
+	@Override
+	public int getValue() {
+		// TODO Auto-generated method stub
+		return value;
+	}
+	
+	public String toString(){
+		StringBuilder sb = new StringBuilder();
+		sb.append(value);
+		return sb.toString() ;
+		
+	}
+	
 
 }
--- PlanningPoker-etsf01/src/poker/CardDeck_Interface.java
+++ PlanningPoker-etsf01/src/poker/CardDeck_Interface.java
@@ -1,10 +1,11 @@
 package poker;
 
 public interface CardDeck_Interface {
-	
-	public Card_Interface chooseCard();
+	//enter the number on the choosen card
+	//If nbr not exists return null
+	public Card_Interface chooseCard(int nbr);
 
-	
+	public Card_Interface choosenCard();
 	
 	
 }
