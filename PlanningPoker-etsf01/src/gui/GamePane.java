9cd309bc8a3be80215d4fb346b111628aea6ac18
Fredrik <adi10fli@student.lu.se>
2013-04-24 11:31:59 +0200

    Skrivit play card

--- PlanningPoker-etsf01/src/gui/GamePane.java
+++ PlanningPoker-etsf01/src/gui/GamePane.java
@@ -1,4 +1,7 @@
 package gui;
+import game.ProgramHandler;
+import gameClient.GameClient;
+
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import com.jgoodies.forms.factories.DefaultComponentFactory;
@@ -8,12 +11,17 @@ import javax.swing.JTextArea;
 import javax.swing.JButton;
 import javax.swing.JList;
 
+import poker.Card;
 import poker.Question;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JScrollPane;
 
 
 public class GamePane extends JPanel {
-	private JTextField questionDescriptionTF;
 	private JLabel questionLabel;
+	private GameClient gameClient;
+	private ProgramHandler pHandler;
 	/**
 	 * Create the panel.
 	 */
@@ -28,49 +36,103 @@ public class GamePane extends JPanel {
 		questionLabel.setBounds(54, 47, 154, 39);
 		add(questionLabel);
 		
-		questionDescriptionTF = new JTextField();
-		questionDescriptionTF.setBounds(54, 97, 241, 220);
-		add(questionDescriptionTF);
-		questionDescriptionTF.setColumns(10);
-		
 		JButton btnNewButton = new JButton("1");
+		btnNewButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				gameClient.ChooseCard(new Card (1));
+			}
+		});
 		btnNewButton.setBounds(54, 395, 49, 39);
 		add(btnNewButton);
 		
 		JButton button = new JButton("2");
+		button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameClient.ChooseCard(new Card (2));
+			}
+		});
 		button.setBounds(101, 395, 49, 39);
 		add(button);
 		
 		JButton button_1 = new JButton("3");
+		button_1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameClient.ChooseCard(new Card (3));
+			}
+		});
 		button_1.setBounds(150, 395, 49, 39);
 		add(button_1);
 		
 		JButton button_2 = new JButton("5");
+		button_2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameClient.ChooseCard(new Card (5));
+			}
+		});
 		button_2.setBounds(199, 395, 49, 39);
 		add(button_2);
 		
 		JButton button_3 = new JButton("13");
+		button_3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameClient.ChooseCard(new Card (13));
+			}
+		});
 		button_3.setBounds(54, 435, 49, 39);
 		add(button_3);
 		
 		JButton button_4 = new JButton("21");
+		button_4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameClient.ChooseCard(new Card (21));
+			}
+		});
 		button_4.setBounds(101, 435, 49, 39);
 		add(button_4);
 		
 		JButton button_5 = new JButton("34");
+		button_5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameClient.ChooseCard(new Card (34));
+			}
+		});
 		button_5.setBounds(150, 435, 49, 39);
 		add(button_5);
 		
 		JButton button_6 = new JButton("55");
+		button_6.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameClient.ChooseCard(new Card (55));
+			}
+		});
 		button_6.setBounds(199, 435, 49, 39);
 		add(button_6);
 		
 		JButton button_7 = new JButton("8");
+		button_7.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameClient.ChooseCard(new Card (8));
+			}
+		});
 		button_7.setBounds(246, 395, 49, 39);
 		add(button_7);
 		
 		JButton button_8 = new JButton("89");
-		button_8.setEnabled(false);
+		button_8.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				gameClient.ChooseCard(new Card (89));
+			}
+		});
 		button_8.setBounds(246, 435, 49, 39);
 		add(button_8);
 		
@@ -85,6 +147,15 @@ public class GamePane extends JPanel {
 		JButton btnNewButton_1 = new JButton("Se resultat");
 		btnNewButton_1.setBounds(341, 351, 104, 23);
 		add(btnNewButton_1);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(31, 95, 214, 267);
+		add(scrollPane);
+		
+		JTextPane textPane = new JTextPane();
+		textPane.setEditable(false);
+		scrollPane.setViewportView(textPane);
+	
 
 	}
 	
@@ -93,5 +164,4 @@ public class GamePane extends JPanel {
 		questionLabel.setText(question.question);
 		questionDescriptionTF.setText(question.questionDescription);
 	}
-
 }
